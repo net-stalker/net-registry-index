631d7ca859471eb3caa845f49367e2b8d3633174
net-illia-stetsenko <illia.stetsenko@netstalker.io>
2023-08-23 14:37:04 +0300

    feature/CU-8678p3kf7: tested get-tag-from-toml.py

--- get-tag-from-toml.py
+++ get-tag-from-toml.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import re
+import toml
 
 def get_ignored_members(manifest_dir):
     registry_ignore = os.path.join(manifest_dir, ".registryignore")
@@ -18,7 +19,7 @@ def get_workspace_members(manifest_dir):
         return cargo_toml['workspace']['members']
 
 def filter_workspace_members_by_ignored(workspace_members, ignored_members):
-    return [member for member in workspace_members if x not in ignored_members]   
+    return [member for member in workspace_members if member not in ignored_members]   
 
 def get_versions_from_toml(manifest_dir, workspace_members):
     versions = []
@@ -33,12 +34,13 @@ def get_current_version_from_changelog(manifest_dir):
     changelog_path = os.path.join(manifest_dir, "CHANGELOG.md")
     ans = ''
     with open(changelog_path, 'r') as changelog:
-        pattern = r"(\[\d+.\d+.\d+\])"
-        prog = re.compile.pattern(pattern)
+        pattern = r"\[\d+.\d+.\d+\]"
+        prog = re.compile(pattern)
         for line in changelog:
-            res = prog.match(line)
+            res = prog.search(line)
             if res == None:
                 continue
+            res = res.group()
             ans = res[1:len(res) - 1]
             break
     
@@ -49,11 +51,13 @@ def get_a_new_tag(members_versions, current_version):
         raise RuntimeError("The versions of the workspace members aren't equal")
     new_version_splitted = members_versions[0].split('.')
     current_version_splitted = current_version.split('.')
-    for i in len(new_version_splitted):
+    for i in range(0, len(new_version_splitted)):
+        if int(current_version_splitted[i]) > int(new_version_splitted[i]):
+            raise RuntimeError("A new version is smaller than the current one")
         if int(current_version_splitted[i]) < int(new_version_splitted[i]):
-            return members_versions[0]
-    raise RuntimeError("A new version is equal or smaller that the current one")
-
+            return members_versions[0]        
+    raise RuntimeError("A new version is equal to the current one")
+    
 def main():
     args = sys.argv[1:]
     if len(args) != 1:
@@ -64,7 +68,7 @@ def main():
     filtered_members = filter_workspace_members_by_ignored(workspace_members, ignored_members)
     current_version = get_current_version_from_changelog(manifest_dir)
     versions = get_versions_from_toml(manifest_dir, filtered_members)
-    return get_a_new_tag(versions, current_version)
+    print(get_a_new_tag(versions, current_version))
 
 
 if __name__ == "__main__":
